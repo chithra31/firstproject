93648e3c593f714dd51736315db7d2acbfad8b18
chithra31 <164135642+chithra31@users.noreply.github.com>
2024-07-15 06:53:33 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product, Viewproduct, Cart
+
+
+admin.site.register(Product)
+admin.site.register(Viewproduct)
+admin.site.register(Cart)
+# Register your models here.
