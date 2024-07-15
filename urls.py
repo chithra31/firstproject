dc6f09f95f35f86c63263c01aa26b4724d93f841
chithra31 <164135642+chithra31@users.noreply.github.com>
2024-07-15 06:52:07 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,19 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
-from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
-    path('', include('loginpage.urls')),
-    path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('loginpage/', views.loginpage, name='loginpage'),
+    path("login/",auth_views.LoginView.as_view(template_name='login.html'),name="login"),
+    path('logout/',views.logout_page,name="logout"),
+    path('register/', views.register, name='register'),
+    path("profile/",views.profile,name="profile"),
+    path('loginpage/<str:name>', views.loginpageview, name='loginpage'),
+    path('addtocart',views.add_to_cart,name="addtocart"),
+    path('cart',views.cart_page,name="cart"),
+    path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
+
+    
+
+] 
